ad05ed35be9bfff98d582a15113e17d7f66618c7
SeungGukYoo <dbtmd878@gmail.com>
2023-05-24 21:23:58 +0900

    amplify 설치 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Amplify } from "aws-amplify";
+import type { AppProps } from "next/app";
+import { config } from "process";
+
+Amplify.configure(config);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const AnyComponent = Component as any;
+  return <AnyComponent {...pageProps} />;
 }
