947608bec11854efcff3cd4f4619c32549d3deec
SeungGukYoo <dbtmd878@gmail.com>
2023-05-29 20:29:00 +0900

    awsconfig 파일 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
+import awsconfig from "@/src/aws-exports";
 import "@/styles/globals.css";
 import { Amplify } from "aws-amplify";
 import type { AppProps } from "next/app";
 import { config } from "process";
 
 Amplify.configure(config);
+Amplify.configure(awsconfig);
 
 export default function App({ Component, pageProps }: AppProps) {
   const AnyComponent = Component as any;
